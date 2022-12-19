0e15549da12deee54c80bad9123bef0c95ef8fae
brandynbennett <brandynlbennett@gmail.com>
2022-12-19 16:15:58 -0700

    initialize gem

---
+++ lib/commanded/version.rb
@@ -0,0 +1,3 @@
+module Commanded
+  VERSION = "0.1.0"
+end
---
+++ spec/commanded_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Commanded do
+  it "has a version number" do
+    expect(Commanded::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
